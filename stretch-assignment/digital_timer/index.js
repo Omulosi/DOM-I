4392afe352a1cd3a43213b9c3f9e39e010137609
Omulosi <mulongojohnpaul@gmail.com>
2019-09-02 21:21:26 +0300

    Comple stretch
    Add a reset button and remove updateTime functio from inside
    timer function (define it in global scope to be used also in
    event listeners)

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -9,29 +9,29 @@ let timerID;
 
 let strTime, msH, msT, msO, secT, secO;
 
+let updateTime = (time, type) =>  {
+  if (type == 'milli') {
+    strTime = `${time}`.length < 3? `0${time}`: `${time}`;
+    [msH, msT, _] = strTime.split('');
+    msHundreds.textContent = msH;
+    msTens.textContent = msT;
+  }
+
+  if (type == 'sec') {
+    strTime = `${time}`.length < 2?`0${time}`: `${time}`;
+    [secT, secO] = strTime.split('');
+    secondOnes.textContent = secO;
+    secondTens.textContent = secT;
+  }
+
+}
+  
 function timer() {
   let secondTens = document.querySelector('#secondTens');
   let secondOnes = document.querySelector('#secondOnes');
   let msHundreds = document.querySelector('#msHundreds');
   let msTens = document.querySelector('#msTens');
 
-  let updateTime = (time, type) =>  {
-    if (type == 'milli') {
-      strTime = `${time}`.length < 3? `0${time}`: `${time}`;
-      [msH, msT, _] = strTime.split('');
-      msHundreds.textContent = msH;
-      msTens.textContent = msT;
-    }
-
-    if (type == 'sec') {
-      strTime = `${sec}`.length < 2?`0${sec}`: `${sec}`;
-      [secT, secO] = strTime.split('');
-      secondOnes.textContent = secO;
-      secondTens.textContent = secT;
-    }
-
-  }
-  
   time = time + STEP;
 
   if (time >= 1000) {
@@ -68,3 +68,19 @@ startBtn.addEventListener('click', (e) => {
   timerID = setInterval(timer, STEP);
   e.target.disabled = 'true';
 });
+
+resetBtn.addEventListener('click', (e) => {
+  let secondTens = document.querySelector('#secondTens');
+  let secondOnes = document.querySelector('#secondOnes');
+  let msHundreds = document.querySelector('#msHundreds');
+  let msTens = document.querySelector('#msTens');
+  let start = document.querySelector('.controls > button:nth-child(1)');
+  start.disabled = false;
+
+  sec = time = 0;
+
+  updateTime(0, 'sec');
+  updateTime(0, 'milli');
+
+  [...document.querySelectorAll('.digits')].forEach(elem => elem.classList.remove('redDigit'));
+})
